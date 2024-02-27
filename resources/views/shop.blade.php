8cec217174390463f5da35b1446c3e86e546ab1a
yvan-nguekam <nguekamyvan4@gmail.com>
2024-02-27 10:16:55 +0100

    pagination on page shop

--- resources/views/shop.blade.php
+++ resources/views/shop.blade.php
@@ -529,36 +529,7 @@
                   @endforeach
 
               </div>
-              {{$products->links()}}
-              {{-- <nav class="page-section">
-                  <ul class="pagination">
-                      <li class="page-item">
-                          <a class="page-link" href="javascript:void(0)" aria-label="Previous"
-                              style="color:#6c757d;">
-                              <span aria-hidden="true">
-                                  <i class="fas fa-chevron-left"></i>
-                              </span>
-                          </a>
-                      </li>
-
-
-                      <li class="page-item active">
-                          <a class="page-link" href="javascript:void(0)">1</a>
-                      </li>
-                      <li class="page-item">
-                          <a class="page-link" href="shop-1.html?page=2">2</a>
-                      </li>
-
-                      <li class="page-item">
-                          <a href="shop-1.html?page=2" class="page-link" aria-label="Next">
-                              <span aria-hidden="true">
-                                  <i class="fas fa-chevron-right"></i>
-                              </span>
-                          </a>
-                      </li>
-
-                  </ul>
-              </nav> --}}
+              {{$products->links("pagination.default")}} 
 
           </div>
       </div>
